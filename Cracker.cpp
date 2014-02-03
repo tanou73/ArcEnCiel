ec8675105fec5b3ce6d2a18d145c028695f6970d
hugo <hugo.chiavenuto@gmail.com>
2014-02-03 14:43:53 +0100

    cracker in progress

---
+++ Cracker.cpp
@@ -0,0 +1,16 @@
+#include "Cracker.h"
+/*
+Cracker::Cracker()
+{
+    //ctor
+}
+
+Cracker::~Cracker()
+{
+    //dtor
+}*/
+
+bool Cracker::cracker( Empreinte y, ArcEnCiel aec, Contexte ctxt, String & clair )
+{
+
+}
---
+++ Cracker.h
@@ -0,0 +1,16 @@
+#ifndef CRACKER_H
+#define CRACKER_H
+
+
+class Cracker
+{
+    public:
+        Cracker();
+        virtual ~Cracker();
+        bool cracker( Empreinte y, ArcEnCiel aec, Contexte ctxt, String & clair );
+
+    protected:
+    private:
+};
+
+#endif // CRACKER_H
